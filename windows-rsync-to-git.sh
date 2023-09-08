2de18df721adb299eb08931e9effcda99ad9b977
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-09-08 16:15:59 -0500

    simplify code

--- windows-rsync-to-git.sh
+++ windows-rsync-to-git.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+CODE_DIR="$HOME/win/AppData/Roaming/Code/User"
+
 # add -n,--dry-run flag if testing
 
 sync_from_home() {
-    rsync --recursive --times --progress --protect-args "$1" .
+    rsync --recursive --times --progress --protect-args "$CODE_DIR/$1" .
 }
 
-sync_from_home "$HOME/win/AppData/Roaming/Code/User/keybindings.json"
-sync_from_home "$HOME/win/AppData/Roaming/Code/User/settings.json"
-sync_from_home "$HOME/win/AppData/Roaming/Code/User/snippets"
+sync_from_home "keybindings.json"
+sync_from_home "settings.json"
+sync_from_home "snippets"
