4e044637aadf3ecd6f4c62719052f4579a2444f2
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2024-04-26 14:45:33 -0500

    change rsync file to macos

--- rsync.sh
+++ rsync.sh
@@ -4,11 +4,9 @@ CODE_DIR=$(dirname -- "$(readlink -f -- "$0")")
 
 sync_from_home() {
     # add -n,--dry-run flag if testing
-    rsync --recursive --times --progress --protect-args "$HOME/win/AppData/Roaming/Code/User/$1" "$CODE_DIR/"
+    rsync --recursive --times --progress --protect-args "$HOME/Library/Application Support/Code/User/$1" "$CODE_DIR/"
 }
 
 sync_from_home "keybindings.json"
 sync_from_home "settings.json"
 sync_from_home "snippets"
-
-./format-files.sh
