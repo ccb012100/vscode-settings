56e1d69045e5dbbfb33d140b45781fd4b4d081ec
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2024-05-24 15:03:03 -0500

    update settings.json

--- rsync.sh
+++ rsync.sh
@@ -2,8 +2,6 @@
 
 CODE_DIR=$(dirname -- "$(readlink -f -- "$0")")
 
-sync_from_home() {
-    # add -n,--dry-run flag if testing
-    rsync --recursive --times --progress --protect-args "$HOME/Library/Application Support/Code/User/$1" "$CODE_DIR/"
-}
+# add -n,--dry-run flag if testing
+rsync --recursive --times --progress --protect-args "$HOME/Library/Application Support/Code/User/settings.json" "$CODE_DIR/"
 
