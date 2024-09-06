1df384c78d12fb39c3657ef166a38c728f6e6350
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2024-09-06 13:57:38 -0500

    edit color-comments-refreshed settings

--- rsync.sh
+++ rsync.sh
@@ -3,5 +3,6 @@
 CODE_DIR=$(dirname -- "$(readlink -f -- "$0")")
 
 # add -n,--dry-run flag if testing
-rsync --recursive --times --progress --protect-args "$HOME/Library/Application Support/Code/User/settings.json" "$CODE_DIR/"
+rsync --recursive --times --progress --protect-args "$HOME/Library/Application Support/Code/User/settings.json" "$CODE_DIR/" || exit 1
 
+code --list-extensions | sort > extensions.txt
