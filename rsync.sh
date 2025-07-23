b03e9e946335232e3807abd8f22ddbb5cfe2851d
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2025-07-23 12:36:57 -0500

    add snippets

--- rsync.sh
+++ rsync.sh
@@ -3,6 +3,7 @@
 CODE_DIR=$(dirname -- "$(readlink -f -- "$0")")
 
 # add -n,--dry-run flag if testing
+rsync --recursive --times --progress --protect-args "$HOME/Library/Application Support/Code/User/snippets" "$CODE_DIR/" || exit 1
 rsync --recursive --times --progress --protect-args "$HOME/Library/Application Support/Code/User/keybindings.json" "$CODE_DIR/" || exit 1
 rsync --recursive --times --progress --protect-args "$HOME/Library/Application Support/Code/User/settings.json" "$CODE_DIR/" || exit 1
 
