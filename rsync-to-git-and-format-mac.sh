81b9405e31869ed5c5a5d354b59b928fb63613e7
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2026-04-13 09:45:08 -0500

    change to mac path

--- rsync-to-git-and-format-mac.sh
+++ rsync-to-git-and-format-mac.sh
@@ -4,7 +4,7 @@ CODE_DIR=$(dirname -- "$(readlink -f -- "$0")")
 
 sync_from_home() {
     # add -n,--dry-run flag if testing
-    rsync --recursive --times --progress --protect-args "$HOME/win/AppData/Roaming/Code/User/$1" "$CODE_DIR/"
+    rsync --recursive --times --progress --protect-args "$HOME/Library/Application Support/Code/User/$1" "$CODE_DIR/"
 }
 
 sync_from_home "keybindings.json"
